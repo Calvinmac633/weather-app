6a8d114f0f31402a066a7e3d0e33593463c6740b
Calvin Macintosh <calvinmac633@gmail.com>
2019-12-27 22:02:05 -0500

    forecast update

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 $(document).ready(function () {
 
-    $("#card1").text(moment().add(1, "days").format('l'))
-    $("#card2").text(moment().add(2, "days").format('l'))
-    $("#card3").text(moment().add(3, "days").format('l'))
-    $("#card4").text(moment().add(4, "days").format('l'))
-    $("#card5").text(moment().add(5, "days").format('l'))
 
-    renderCities()
+    var card1 = $("#card1").text(moment().add(1, "days").format('l'))
+    var card2 = $("#card2").text(moment().add(2, "days").format('l'))
+    var card3 = $("#card3").text(moment().add(3, "days").format('l'))
+    var card4 = $("#card4").text(moment().add(4, "days").format('l'))
+    var card5 = $("#card5").text(moment().add(5, "days").format('l'))
+
 
     var cityArr = [];
+    // cityArr.push(localStorage.getItem("city names"));
+
+
 
     $(".searchButton").on("click", function (element) {
         event.preventDefault();
+        // var cityArr = [(localStorage.getItem("city names"))];
+
         var cityName = $(".inputBox").val().trim();
         var cityListItem = $("<button>").html(cityName).attr("class", "cityList")
         $(".city-container").prepend(cityListItem)
@@ -31,46 +36,100 @@ $(document).ready(function () {
         var card2Humidity = document.getElementsByClassName("card-group")[0].childNodes[1].children[0].children[2].innerText
 
 
+        function generateDisplay() {
+            // api.openweathermap.org/data/2.5/weather?q=London
+            var APIKey = "166a433c57516f51dfab1f7edaed8413";
+            var cityName = $("#cityNameDisplay").text()
+            console.log(cityName)
+            var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&cnt=6&appid=" + APIKey;
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response)
+                $("#temperature").text("Temperature: " + Math.round(response.main.temp) + "°F")
+                $("#humidity").text("Humidity: " + response.main.humidity + "%")
+                $("#windSpeed").text("Wind Speed: " + response.wind.speed)
+                $("#uvIndex").text("UV Index: " + response.main)
+            });
+        }
+        generateDisplay();
+
+
+
+
 
         var APIKey = "166a433c57516f51dfab1f7edaed8413";
         var cityName = $("#cityNameDisplay").text()
         console.log(cityName)
-        var queryURL = "https://api.openweathermap.org/data/2.5/find?q=" + cityName + "&units=imperial&appid=" + APIKey;
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + cityName + "&units=imperial&cnt=6&appid=" + APIKey;
+
+        // api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=xml&units=metric&cnt=7
 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
             console.log(response)
-            console.log(response.list[0])
+            console.log()
+            $(".card-text-temp-1").html("Temperature:" + "<br>" + "High: " + Math.round(response.list[1].temp.max)+ "°F" + " || Lo: " + Math.round(response.list[1].temp.min) + "°F")
+            $(".card-text-hum-1").html("Humidity: " + response.list[1].humidity + "%")
+            $(".card-text-temp-2").html("Temperature:" + "<br>" + "High: " + Math.round(response.list[2].temp.max)+ "°F" + " || Lo: " + Math.round(response.list[2].temp.min) + "°F")
+            $(".card-text-hum-2").html("Humidity: " + response.list[2].humidity + "%")
+            $(".card-text-temp-3").html("Temperature:" + "<br>" + "High: " + Math.round(response.list[3].temp.max)+ "°F" + " || Lo: " + Math.round(response.list[3].temp.min) + "°F")
+            $(".card-text-hum-3").html("Humidity: " + response.list[3].humidity + "%")
+            $(".card-text-temp-4").html("Temperature:" + "<br>" + "High: " + Math.round(response.list[4].temp.max)+ "°F" + " || Lo: " + Math.round(response.list[4].temp.min) + "°F")
+            $(".card-text-hum-4").html("Humidity: " + response.list[4].humidity + "%")
+            $(".card-text-temp-5").html("Temperature:" + "<br>" + "High: " + Math.round(response.list[5].temp.max)+ "°F" + " || Lo: " + Math.round(response.list[5].temp.min) + "°F")
+            $(".card-text-hum-5").html("Humidity: " + response.list[5].humidity + "%")
 
+            var cards = []
 
             var conditions = response.list[0].weather[0].main
 
+            function attachIcon(element) {
+                if (conditions === "Clouds") {
+                    image = $("<i>").addClass("fas fa-cloud-sun")
+                    element.append(image)
+                } else if (conditions === "Rain") {
+                    image = $("<i>").addClass("fas fa-cloud-rain")
+                    element.append(image)
+                } else if (conditions === "Clear") {
+                    image = $("<i>").addClass("far fa-sun")
+                    element.append(image)
+                } else if (conditions === "Mist") {
+                    image = $("<i>").addClass("fas fa-smog")
+                    element.append(image)
+                }
+            }
+
             if (conditions === "Clouds") {
                 image = $("<i>").addClass("fas fa-cloud-sun")
-                $("#cityNameDisplay").text(response.list[0].name + " (" + moment().format('l') + ") ")
+                $("#cityNameDisplay").text(response.city.name + " (" + moment().format('l') + ") ")
                 $("#cityNameDisplay").append(image);
                 $("body").attr("style", "background-image: url(https://cdn.solace.com/wp-content/uploads/2019/01/bg-clouds.jpg)")
                 console.log(image)
             } else if (conditions === "Rain") {
                 image = $("<i>").addClass("fas fa-cloud-rain")
-                $("#cityNameDisplay").text(response.list[0].name + " (" + moment().format('l') + ") ")
+                $("#cityNameDisplay").text(response.city.name + " (" + moment().format('l') + ") ")
                 $("#cityNameDisplay").append(image);
                 $("body").attr("style", "background-image: url(https://bluestemamphitheater.org/wp-content/uploads/blog-rain-or-shine.jpg)")
             } else if (conditions === "Clear") {
                 image = $("<i>").addClass("far fa-sun")
-                $("#cityNameDisplay").text(response.list[0].name + " (" + moment().format('l') + ") ")
+                $("#cityNameDisplay").text(response.city.name + " (" + moment().format('l') + ") ")
                 $("#cityNameDisplay").append(image);
                 $("body").attr("style", "background-image: url(https://external-preview.redd.it/PzZiabszp8qatJxLKE_z9XbERpa0jEVe0VFImlQbqic.jpg?auto=webp&s=301afde517afe860f1c648bf7842eda25582f876)")
+            } else if (conditions === "Mist") {
+                image = $("<i>").addClass("fas fa-smog")
+                $("#cityNameDisplay").text(response.city.name + " (" + moment().format('l') + ") ")
+                $("#cityNameDisplay").append(image);
+                $("body").attr("style", "background-image: url(https://ianwcanoe.files.wordpress.com/2013/01/warm-mist-landscape.jpg)")
             }
 
 
+            
 
-            $("#temperature").text("Temperature: " + response.list[0].main.temp)
-            $("#humidity").text("Humidity: " + response.list[0].main.humidity)
-            $("#windSpeed").text("Wind Speed: " + response.list[0].wind.speed)
-            $("#uvIndex").text("UV Index: " + response.list[0].main)
 
 
 
@@ -80,20 +139,19 @@ $(document).ready(function () {
 
 
 
-
-
     })
 
-function renderCities() {
-    var cityNames = JSON.parse(localStorage.getItem("city names"));
-    console.log(cityNames)
-    $.each(cityNames, function(index, value) {
-        var cityListItem = $("<button>").html(value).attr("class", "cityList")
-        $(".city-container").prepend(cityListItem)
-        $("#cityNameDisplay").html(value)
-    })
-}
+    function renderCities() {
+        var cityNames = JSON.parse(localStorage.getItem("city names"));
+        console.log(cityNames)
+        $.each(cityNames, function (index, value) {
+            var cityListItem = $("<button>").html(value).attr("class", "cityList")
+            $(".city-container").prepend(cityListItem)
+            $("#cityNameDisplay").html(value + " (" + moment().format('l') + ") ")
+        })
+    }
 
+    
 
 
 
